dd38410282d65b703774289de4aa8b0c95536656
coding-datul <johnherszel2@gmail.com>
2026-05-29 17:41:59 +0800

    Restore lab act 1 source files

---
+++ datul-client/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-Header'>
+        <h1>Welcome To My React App!</h1>
+        <p>
+          Name: John Herszel D. Datul<br /> 
+          Section: INF-235<br />
+          Github Link: <a href='https://github.com/coding-datul'>Github</a>
+        
+        </p>
+      </header>
+    </div>
+
+  );
+
+}
+  
+
+export default App;
